953aa339ceae92ff621feb2ba6f8faf0a1172d6e
Abdalrahman Mohamed <abdamoha@lab1r4s8.42abudhabi.ae>
2023-12-17 22:32:45 +0400

    bug to fix in validateDirectives

--- main.cpp
+++ main.cpp
@@ -49,85 +49,85 @@ void	validateDirectiveParent(const std::string &key, const std::string &parentNa
 		}
 }
 
-void	validateDirectiveCodes(DirectiveNode *directiveNode)
-{
-	if (directiveNode->getKey() == "return")
-	{
-		try {
-			int code = std::stoi(directiveNode->getValues()[0]);
-			if (code < 100 || code > 599)
-				throw (std::runtime_error("invalid return code " + directiveNode->getValues()[0] + " in \"return\" directive"));
-		}
-		catch (std::exception &e) {
-			throw (std::runtime_error("invalid return code " + directiveNode->getValues()[0] + " in \"return\" directive"));
-		}
-	}
-	else if (directiveNode->getKey() == "error_page")
-	{
-		try {
-			int code = std::stoi(directiveNode->getValues()[0]);
-			if (code < 300 || code > 599)
-				throw (std::runtime_error("invalid return code " + directiveNode->getValues()[0] + " in \"error_page\" directive"));
-		}
-		catch (std::exception &e) {
-			throw (std::runtime_error("invalid return code " + directiveNode->getValues()[0] + " in \"error_page\" directive"));
-		}
-	}
+// void	validateDirectiveCodes(DirectiveNode *directiveNode)
+// {
+// 	if (directiveNode->getKey() == "return")
+// 	{
+// 		try {
+// 			int code = std::stoi(directiveNode->getValues()[0]);
+// 			if (code < 100 || code > 599)
+// 				throw (std::runtime_error("invalid return code " + directiveNode->getValues()[0] + " in \"return\" directive"));
+// 		}
+// 		catch (std::exception &e) {
+// 			throw (std::runtime_error("invalid return code " + directiveNode->getValues()[0] + " in \"return\" directive"));
+// 		}
+// 	}
+// 	else if (directiveNode->getKey() == "error_page")
+// 	{
+// 		try {
+// 			int code = std::stoi(directiveNode->getValues()[0]);
+// 			if (code < 300 || code > 599)
+// 				throw (std::runtime_error("invalid return code " + directiveNode->getValues()[0] + " in \"error_page\" directive"));
+// 		}
+// 		catch (std::exception &e) {
+// 			throw (std::runtime_error("invalid return code " + directiveNode->getValues()[0] + " in \"error_page\" directive"));
+// 		}
+// 	}
 	
-}
+// }
 
-void    validateDirectives(const ConfigNode *node)
-{
-	std::vector<std::string> tmp;
-	std::map<std::string, std::pair<int , int> >  possibleDirs;
-	possibleDirs["root"] = std::make_pair(OneArg, Independent); /*only one*/
-	possibleDirs["listen"] = std::make_pair(OneArg, ParentNeeded); /*only one*/
-	possibleDirs["autoindex"] = std::make_pair(OneArg, Independent); /*only one*/
-	possibleDirs["server_name"] = std::make_pair(OneArg, ParentNeeded); /*only one*/
-	possibleDirs["client_max_body_size"] = std::make_pair(OneArg, Independent); /*only one*/
-	possibleDirs["error_page"] = std::make_pair(TwoArgs, Independent); /*two or more*/
-	possibleDirs["try_files"] = std::make_pair(TwoOrMoreArgs, ParentNeeded); /*two or more*/
-	possibleDirs["rewrite"] = std::make_pair(TwoArgs, ParentNeeded); /* only 2*/
-	possibleDirs["index"] = std::make_pair(OneOrMoreArgs, Independent); /*one or more*/
-	possibleDirs["return"] = std::make_pair(OneOrTwoArgs, ParentNeeded); /*one or two*/
+// void    validateDirectives(const ConfigNode *node)
+// {
+// 	std::vector<std::string> tmp;
+// 	std::map<std::string, std::pair<int , int> >  possibleDirs;
+// 	// possibleDirs["root"] = std::make_pair(OneArg, Independent); /*only one*/
+// 	// possibleDirs["listen"] = std::make_pair(OneArg, ParentNeeded); /*only one*/
+// 	// possibleDirs["autoindex"] = std::make_pair(OneArg, Independent); /*only one*/
+// 	// possibleDirs["server_name"] = std::make_pair(OneArg, ParentNeeded); /*only one*/
+// 	// possibleDirs["client_max_body_size"] = std::make_pair(OneArg, Independent); /*only one*/
+// 	// possibleDirs["error_page"] = std::make_pair(TwoArgs, Independent); /*two or more*/
+// 	// possibleDirs["try_files"] = std::make_pair(TwoOrMoreArgs, ParentNeeded); /*two or more*/
+// 	// possibleDirs["rewrite"] = std::make_pair(TwoArgs, ParentNeeded); /* only 2*/
+// 	// possibleDirs["index"] = std::make_pair(OneOrMoreArgs, Independent); /*one or more*/
+// 	// possibleDirs["return"] = std::make_pair(OneOrTwoArgs, ParentNeeded); /*one or two*/
 
-	if (node->getType() == Context)
-	{
-		ContextNode *contextNode = (ContextNode *)(node);
-		for (int i = 0; i < contextNode->getNumChildren(); i++)
-			validateDirectives(contextNode->getChildren()[i]);
-	}
-	if (node->getType() == Directive)
-	{
-		DirectiveNode *directive = (DirectiveNode *)(node);
-		std::map<std::string, std::pair<int, int> >::iterator it = possibleDirs.find(directive->getKey());
-		if (it != possibleDirs.end())
-		{
-			 ContextNode *parentNode = ( ContextNode *)(directive->getParent());
+// 	if (node->getType() == Context)
+// 	{
+// 		ContextNode *contextNode = (ContextNode *)(node);
+// 		for (int i = 0; i < contextNode->getNumChildren(); i++)
+// 			validateDirectives(contextNode->getChildren()[i]);
+// 	}
+// 	if (node->getType() == Directive)
+// 	{
+// 		DirectiveNode *directive = (DirectiveNode *)(node);
+// 		std::map<std::string, std::pair<int, int> >::iterator it = possibleDirs.find(directive->getKey());
+// 		if (it != possibleDirs.end())
+// 		{
+// 			 ContextNode *parentNode = ( ContextNode *)(directive->getParent());
 			
-			if (it->second.second == ParentNeeded) // validate parent
-				validateDirectiveParent(it->first, parentNode->getName());
+// 			if (it->second.second == ParentNeeded) // validate parent
+// 				validateDirectiveParent(it->first, parentNode->getName());
 
 
-			if (it->second.first == OneArg && directive->getValueCount() != 1)
-				throw (std::runtime_error("Invalid number of arguments in \"" + directive->getKey() + "\" directive"));
-			else if (it->second.first == TwoOrMoreArgs && directive->getValueCount() < 2)
-				throw (std::runtime_error("Invalid number of arguments in \"" + directive->getKey() + "\" directive"));
-			else if (it->second.first == OneOrMoreArgs && directive->getValueCount() < 1)
-				throw (std::runtime_error("Invalid number of arguments in \"" + directive->getKey() + "\" directive"));
-			else if (it->second.first == TwoArgs && directive->getValueCount() != 2)
-				throw (std::runtime_error("Invalid number of arguments in \"" + directive->getKey() + "\" directive"));
-			else if (it->second.first == OneOrTwoArgs && (directive->getValueCount() < 1 || directive->getValueCount() > 2))
-				throw (std::runtime_error("Invalid number of arguments in \"" + directive->getKey() + "\" directive"));
+// 			if (it->second.first == OneArg && directive->getValueCount() != 1)
+// 				throw (std::runtime_error("Invalid number of arguments in \"" + directive->getKey() + "\" directive"));
+// 			else if (it->second.first == TwoOrMoreArgs && directive->getValueCount() < 2)
+// 				throw (std::runtime_error("Invalid number of arguments in \"" + directive->getKey() + "\" directive"));
+// 			else if (it->second.first == OneOrMoreArgs && directive->getValueCount() < 1)
+// 				throw (std::runtime_error("Invalid number of arguments in \"" + directive->getKey() + "\" directive"));
+// 			else if (it->second.first == TwoArgs && directive->getValueCount() != 2)
+// 				throw (std::runtime_error("Invalid number of arguments in \"" + directive->getKey() + "\" directive"));
+// 			else if (it->second.first == OneOrTwoArgs && (directive->getValueCount() < 1 || directive->getValueCount() > 2))
+// 				throw (std::runtime_error("Invalid number of arguments in \"" + directive->getKey() + "\" directive"));
 
-			validateDirectiveCodes(directive);
-		}
-		else {
-			throw (std::runtime_error("Unknown directive \"" + directive->getKey() + "\""));
+// 			validateDirectiveCodes(directive);
+// 		}
+// 		else {
+// 			throw (std::runtime_error("Unknown directive \"" + directive->getKey() + "\""));
 		
-		}
-	}
-}
+// 		}
+// 	}
+// }
 
 void tokenize(const std::string &input, std::vector<std::string> &tokens)
 {
@@ -174,6 +174,7 @@ int main()
         std::vector<std::string>	tokens;
         std::vector<std::string>::iterator	it;
         std::string							tmp;
+		LogicValidate						logicValidation;
 
         if (!ifs.is_open())
         {
@@ -203,9 +204,11 @@ int main()
 		ConfigNode *root = TreeBuilder::builder(NULL, it, end);
 
 		// logicValidation(root);
-		validateDirectives(root);
+		// validateDirectives(root);
+		logicValidation.validateDirectives(root);
 
-		std::cout << "Success" << std::endl;
+		std::cout << "nginx 2.0 : the configuration file /etc/nginx/nginx.conf syntax is ok" << std::endl
+				  << "nginx 2.0: configuration file /etc/nginx/nginx.conf test is successful" << std::endl;
 		return (0);
     }
     catch (std::exception &e)
