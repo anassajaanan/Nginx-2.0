27509480f5bf20a42bd2a5333ec36446c98d7ae5
anassajaanan <p131047727@taalim.ma>
2023-12-18 09:14:43 +0400

    Remove extra headers from files

--- main.cpp
+++ main.cpp
@@ -1,24 +1,4 @@
-#include "ConfigNode.hpp"
-#include "ConfigParser.hpp"
-#include "DirectiveNode.hpp"
-#include "ContextNode.hpp"
-#include <cstddef>
-#include <regex>
-#include <sstream>
-#include <fstream>
-#include <iostream>
-#include <stdexcept>
-#include <string>
-#include <sys/_types/_size_t.h>
-#include <vector>
-#include <map>
-#include <stack>
-#include <set>
 
-
-#include "TreeBuilder.hpp"
-#include "SyntaxValidator.hpp"
-#include "LogicValidator.hpp"
 
 #include "ConfigParser.hpp"
 
@@ -28,7 +8,7 @@ int main()
 {
     try
     {
-        ConfigParser	parser("nginx.conf");
+        ConfigParser	parser("../../Desktop");
 
 		std::cout << "nginx 2.0 : the configuration file /etc/nginx/nginx.conf syntax is ok" << std::endl
 				  << "nginx 2.0: configuration file /etc/nginx/nginx.conf test is successful" << std::endl;
