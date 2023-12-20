3c73912dcc12c47a008661bbcf4a944384ff41cb
anassajaanan <p131047727@taalim.ma>
2023-12-20 18:41:24 +0400

    Implement TryFilesconfig class with setTryFiles setter in ServerConfig

--- ServerConfig.hpp
+++ ServerConfig.hpp
@@ -2,11 +2,14 @@
 
 
 #pragma once
+#include "TreeBuilder.hpp"
 #ifndef SERVERCONFIG_HPP
 # define SERVERCONFIG_HPP
 
 #include "ContextNode.hpp"
 #include "DirectiveNode.hpp"
+#include "TryFilesConfig.hpp"
+
 #include <sstream>
 #include <string>
 
@@ -17,14 +20,26 @@ class ServerConfig
 
 private:
 
-	int			port;
-	std::string	ipAddress;
+	int				port;
+	std::string		ipAddress;
+	std::string		serverName;
+	TryFilesConfig	tryFiles;
 
 
 public:
+	// set Listen directive
 	void	setListen(const std::string &listenValue);
 	bool	isValidPort(const std::string &port);
 	bool	isValidIPv4();
+
+	// set ServerName directive
+	void	setServerName(const std::string &serverNameValue);
+
+	// set TryFiles directive
+	void	setTryFiles(const std::vector<std::string> &tryFilesValue);
+	void	processFallbackStatusCode(const std::string &statusCode);
+
+
 
 
 };
