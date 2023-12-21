483bbf25f28ab1b58ca3741d6c135a0b6fe16c98
anassajaanan <p131047727@taalim.ma>
2023-12-21 09:36:31 +0400

    Implemet setRewriteDirective in ServerConfig

---
+++ RewriteDirective.cpp
@@ -0,0 +1,20 @@
+#include "RewriteDirective.hpp"
+
+
+
+RewriteDirective::RewriteDirective(const std::string &pattern, const std::string &substitution)
+	: pattern(pattern), substitution(substitution) { }
+
+
+RewriteDirective::~RewriteDirective() { }
+
+
+void	RewriteDirective::setPattern(const std::string &pattern)
+{
+	this->pattern = pattern;
+}
+
+void	RewriteDirective::setSubstitution(const std::string &substitution)
+{
+	this->substitution = substitution;
+}
