58050e7df0d3b3cd30ec846537163a3c4f555f71
Anas Ajaanan <aajaanan@lab2r4s10.42abudhabi.ae>
2023-12-23 13:51:09 +0400

    Implement BaseConfig class as Base Class for Server/Location Config, and fix the duplicated locations

--- ServerConfig.cpp
+++ ServerConfig.cpp
@@ -1,33 +1,13 @@
 #include "ServerConfig.hpp"
-#include "ConfigNode.hpp"
-#include "ContextNode.hpp"
 #include "LocationConfig.hpp"
-#include <sys/_types/_size_t.h>
 
-ServerConfig::ServerConfig()
-{
-	this->port = 80;
-	this->ipAddress = "0.0.0.0";
-	this->root = "/var/www/html";
-	this->index = "index.html";
-	this->serverName = "";
-	this->autoindex = "off";
-	clientMaxBodySize = DEFAULT_CLIENT_MAX_BODY_SIZE;
-}
-
+ServerConfig::ServerConfig() { }
 
 ServerConfig::ServerConfig(const std::string &rootValue, const std::string &indexValue,
 				const std::string &autoindexValue, const std::string &client_max_body_size,
 				const std::vector<DirectiveNode *> &errorPagesDirectives)
 {
-	this->port = 80;
-	this->ipAddress = "0.0.0.0";
-	this->root = "/var/www/html";
-	this->index = "index.html";
-	this->serverName = "";
-	this->autoindex = "off";
-	clientMaxBodySize = DEFAULT_CLIENT_MAX_BODY_SIZE;
-	
+	setDefaultValues();
 	setRoot(rootValue);
 	setIndex(indexValue);
 	setAutoindex(autoindexValue);
@@ -37,6 +17,17 @@ ServerConfig::ServerConfig(const std::string &rootValue, const std::string &inde
 		const std::vector<std::string> &values = errorPagesDirectives[i]->getValues();
 		setErrorPage(errorPagesDirectives[i]->getKey(), values[0]);
 	}
+}
+
+void	ServerConfig::setDefaultValues()
+{
+	this->port = DEFAULT_SERVER_PORT;
+	this->ipAddress = DEFAULT_SERVER_IP;
+	this->root = DEFAULT_SERVER_ROOT;
+	this->index = DEFAULT_SERVER_INDEX;
+	this->serverName = DEFAULT_SERVER_NAME;
+	this->autoindex = DEFAULT_SERVER_AUTOINDEX;
+	this->clientMaxBodySize = DEFAULT_CLIENT_MAX_BODY_SIZE;
 }
 
 
@@ -100,178 +91,34 @@ void	ServerConfig::setServerName(const std::string &serverNameValue)
 	this->serverName = serverNameValue;
 }
 
-void	ServerConfig::setTryFiles(const std::vector<std::string> &tryFilesValue)
-{
-	size_t i;
-	for (i = 0; i < tryFilesValue.size() - 1; i++)
-		this->tryFiles.addPath(tryFilesValue[i]);
-	if (tryFilesValue[i][0] == '=')
-		processFallbackStatusCode(tryFilesValue[i]);
-	else
-		this->tryFiles.setFallBackUri(tryFilesValue[i]);
-}
-
-void	ServerConfig::processFallbackStatusCode(const std::string &statusCode)
-{
-	std::string code = statusCode.substr(1);
-	if (code.empty() || code.size() > 3)
-		throw std::runtime_error("invalid code in \"try_files\" directive: \"" + statusCode + "\"");
-	for (size_t j = 0; j < code.size(); j++)
-	{
-		if (!std::isdigit(code[j]))
-			throw std::runtime_error("invalid code in \"try_files\" directive: \"" + statusCode + "\"");
-	}
-	int codeInt = std::stoi(code);
-	if (codeInt < 300 || codeInt > 599)
-		throw std::runtime_error("invalid code in \"try_files\" directive: \"" + statusCode + "\"" + " (must be between 300 and 599)");
-	this->tryFiles.setFallBackStatusCode(codeInt);
-}
-
-
-void	ServerConfig::setReturn(const std::vector<std::string> &returnValue)
-{
-	if (returnValue.size() == 2)
-		this->returnDirective.setResponseTextOrUrl(returnValue[1]);
-	std::string code = returnValue[0];
-	if (code.empty() || code.size() > 3)
-		throw std::runtime_error("invalid code in \"return\" directive: \"" + code + "\"");
-	for (size_t j = 0; j < code.size(); j++)
-	{
-		if (!std::isdigit(code[j]))
-			throw std::runtime_error("invalid code in \"return\" directive: \"" + code + "\"");
-	}
-	int codeInt = std::stoi(code);
-	if (codeInt < 300 || codeInt > 599)
-		throw std::runtime_error("invalid code in \"return\" directive: \"" + code + "\"" + " (must be between 300 and 599)");
-	this->returnDirective.setStatusCode(codeInt);
-}
-
-void	ServerConfig::setRewrite(const std::vector<std::string> &rewriteValue)
-{
-	this->rewriteDirective.setPattern(rewriteValue[0]);
-	this->rewriteDirective.setSubstitution(rewriteValue[1]);
-}
-
-void		ServerConfig::setIndex(const std::string &indexValue)
-{
-	this->index = indexValue;
-}
-
-void	ServerConfig::setRoot(const std::string &rootValue)
-{
-	if (!rootValue.empty())
-		this->root = rootValue;
-}
-
-
-
-bool	ServerConfig::isValidAutoindex(const std::string &autoindexValue)
-{
-	if (autoindexValue.empty() || (autoindexValue != "on" && autoindexValue != "off"))
-		throw (std::runtime_error("invalid value \"" + autoindexValue + "\" in \"autoindex\" directive, it must be \"on\" or \"off\""));
-	return (true);
-}
-
-void	ServerConfig::setAutoindex(const std::string &autoindexValue)
-{
-	isValidAutoindex(autoindexValue);
-	this->autoindex = autoindexValue;
-}
-
-
-
-void	ServerConfig::splitValueAndUnit(const std::string &bodySize, std::string &value, std::string &unit)
-{
-	size_t pos = bodySize.find_first_not_of("0123456789");
-	if (pos != std::string::npos)
-	{
-		value = bodySize.substr(0, pos);
-		unit = bodySize.substr(pos);
-	}
-	else
-		value = bodySize;
-}
-
-size_t	ServerConfig::safeStringToSizeT(const std::string &bodySize, const std::string value)
-{
-	try
-	{
-		return (std::stoull(value));
-		
-	}
-	catch (const std::exception &e)
-	{
-		throw (std::runtime_error("invalid value \"" + bodySize + "\" in \"client_max_body_size\" directive"));
-	}
-}
-
-
-void	ServerConfig::setClientMaxBodySize(const std::string &bodySize)
-{
-	size_t multiplier = 1; // in case there is no unit (bytes by default)
-	size_t numericValue, totalSize;
-	std::string value, unit;
-
-	splitValueAndUnit(bodySize, value, unit);
-
-	if (value.empty() || unit.size() > 1)
-		throw (std::runtime_error("invalid value \"" + bodySize + "\" in \"client_max_body_size\" directive"));
-	if (unit.size() == 1 && unit.find_first_not_of("kKmMgG") != std::string::npos)
-		throw (std::runtime_error("invalid value \"" + bodySize + "\" in \"client_max_body_size\" directive"));
-	if (unit == "k" || unit == "K")
-		multiplier = 1000;
-	else if (unit == "m" || unit == "M")
-		multiplier = 1000000;
-	else if (unit == "g" || unit == "G")
-		multiplier = 1000000000;
-	
-	numericValue = safeStringToSizeT(bodySize, value);
-	totalSize = numericValue * multiplier;
-	// check for overflow
-	if (totalSize / multiplier != numericValue)
-		throw (std::runtime_error("invalid value \"" + bodySize + "\" in \"client_max_body_size\" directive"));
-}
-
-void	ServerConfig::setErrorPage(const std::string &statusCode, const std::string &uri)
-{
-	if (statusCode.empty() || statusCode.size() > 3)
-		throw std::runtime_error("invalid code in \"error_page\" directive: \"" + statusCode + "\"");
-	for (size_t j = 0; j < statusCode.size(); j++)
-	{
-		if (!std::isdigit(statusCode[j]))
-			throw std::runtime_error("invalid code in \"error_page\" directive: \"" + statusCode + "\"");
-	}
-	int codeInt = std::stoi(statusCode);
-	if (codeInt < 300 || codeInt > 599)
-		throw std::runtime_error("invalid code in \"error_page\" directive: \"" + statusCode + "\"" + " (must be between 300 and 599)");
-	errorPages[codeInt] = uri;
-}
-
 void	ServerConfig::addLocation(ContextNode *locationNode)
 {
+	if (locations.find(locationNode->getPath()) != locations.end())
+		throw std::runtime_error("duplicate location \"" + locationNode->getPath() + "\" in Config file");
+
 	LocationConfig location(*this);
 	const std::vector<ConfigNode *> &locationChildren = locationNode->getChildren();
 	for (size_t i = 0; i < locationChildren.size(); i++)
 	{
 		if (locationChildren[i]->getType() == Directive)
 		{
-			DirectiveNode *locationDirective = static_cast<DirectiveNode *>(locationChildren[i]);
-			if (locationDirective->getKey() == "root")
-				location.setRoot(locationDirective->getValues()[0]);
-			else if (locationDirective->getKey() == "index")
-				location.setIndex(locationDirective->getValues()[0]);
-			else if (locationDirective->getKey() == "autoindex")
-				location.setAutoindex(locationDirective->getValues()[0]);
-			else if (locationDirective->getKey() == "client_max_body_size")
-				location.setClientMaxBodySize(locationDirective->getValues()[0]);
-			else if (locationDirective->getKey() == "error_page")
-				location.setErrorPage(locationDirective->getKey(), locationDirective->getValues()[0]);
-			else if (locationDirective->getKey() == "try_files")
-				location.setTryFiles(locationDirective->getValues());
-			else if (locationDirective->getKey() == "return")
-				location.setReturn(locationDirective->getValues());
-			else if (locationDirective->getKey() == "rewrite")
-				location.setRewrite(locationDirective->getValues());
+			DirectiveNode *directive = static_cast<DirectiveNode *>(locationChildren[i]);
+			if (directive->getKey() == "root")
+				location.setRoot(directive->getValues()[0]);
+			else if (directive->getKey() == "index")
+				location.setIndex(directive->getValues()[0]);
+			else if (directive->getKey() == "autoindex")
+				location.setAutoindex(directive->getValues()[0]);
+			else if (directive->getKey() == "client_max_body_size")
+				location.setClientMaxBodySize(directive->getValues()[0]);
+			else if (directive->getKey() == "error_page")
+				location.setErrorPage(directive->getKey(), directive->getValues()[0]);
+			else if (directive->getKey() == "try_files")
+				location.setTryFiles(directive->getValues());
+			else if (directive->getKey() == "return")
+				location.setReturn(directive->getValues());
+			else if (directive->getKey() == "rewrite")
+				location.setRewrite(directive->getValues());
 		}
 	}
 	locations[locationNode->getPath()] = location;
