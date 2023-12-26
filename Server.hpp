7eb31efd8ddb71d939356f62330e080b7fa6939b
Anas Ajaanan <aajaanan@lab2r3s8.42abudhabi.ae>
2023-12-26 08:27:59 +0400

    Implement a basic Server Class

--- Server.hpp
+++ Server.hpp
@@ -5,38 +5,34 @@
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
+#include "ServerConfig.hpp"
+
+#include <fcntl.h>
 #include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-#include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <sys/types.h>
-#include <sys/event.h>
-#include <sys/time.h>
-#include <poll.h>
-#include <fcntl.h>
-#include <sys/types.h>
+#include <arpa/inet.h>
 
-#include "ServerConfig.hpp"
+
+
+#define SERVER_BACKLOG 30
 
 
 class Server
 {
 private:
-	ServerConfig	&config;
-	int				socket;
-	static int		kq;
+	ServerConfig		&_config;
+	int					_socket;
+	int					_kq;
+	struct sockaddr_in	_serverAddr;
 
 public:
 	Server(ServerConfig &config, int kq);
 	~Server();
 
-
-
-
-
-
+	void	createServerSocket();
+	void	setSocketToNonBlocking();
+	void	bindAndListen();
 
 
 
