258e222505ba2c6563800a8dd20ef162f89b4cf9
tukka25 <aboodytukka@gmail.com>
2024-02-03 21:11:16 +0400

    added

--- content/website/cgi-bin/chatbot.py
+++ content/website/cgi-bin/chatbot.py
@@ -1,9 +1,1 @@
 #! /usr/bin/env pyhton3
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
-
-chatbot = ChatBot('Ron Obvious')
-
-trainer.train("chatterbot.corpus.english")
-
-chatbot.get_response("Hello, how are you today?")
